acdd382806980600c490a96cc9b57d00058182fb
ikeda042 <ikeda042main@gmail.com>
2026-01-29 15:43:56 +0900

    Set x-axis limits for FITC aggregation ratio plot

--- backend/app/bulk_engine/crud.py
+++ backend/app/bulk_engine/crud.py
@@ -1164,6 +1164,7 @@ def _build_fitc_aggregation_ratio_plot(
 ) -> bytes:
     fig, ax = plt.subplots(figsize=(4.6, 4.2), dpi=180)
     ax.bar([0], [ratio], color="#2c7a7b", width=0.25)
+    ax.set_xlim(-0.6, 0.6)
     ax.set_ylim(0, 1)
     ax.set_xticks([0])
     ax.set_xticklabels([label_text])
