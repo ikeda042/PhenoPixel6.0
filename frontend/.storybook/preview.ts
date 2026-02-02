d93bef911136cc2ff5592526fc07554c84f09071
ikeda042 <ikeda042main@gmail.com>
2026-02-03 04:04:56 +0900

    feat: add Storybook scripts and dependencies for enhanced UI development

---
+++ frontend/.storybook/preview.ts
@@ -0,0 +1,34 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import type { FC } from 'react'
+import '@fontsource/noto-sans-jp/400.css'
+import '@fontsource/noto-sans-jp/600.css'
+import '@fontsource/noto-sans-jp/700.css'
+import '../src/index.css'
+import system from '../src/theme'
+
+if (typeof document !== 'undefined') {
+  const root = document.documentElement
+  root.classList.add('light')
+  root.classList.remove('dark')
+}
+
+const preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+  decorators: [
+    (Story: FC) => (
+      <ChakraProvider value={system}>
+        <Story />
+      </ChakraProvider>
+    ),
+  ],
+}
+
+export default preview
